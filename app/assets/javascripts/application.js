413975a6b3e2e34d850ccbad58fd3c00e4c63edd
German Garcia <german.garcia.d@gmail.com>
2011-05-07 15:47:35 -0500

    Finishing home design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+function initializeMap() {
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 8,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+}
