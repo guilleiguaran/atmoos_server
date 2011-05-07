fd5e9f96f95c61a4713ff23f01582c8dae0fc44e
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-05-06 21:19:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtmoosServer::Application.config.secret_token = '2f8a0de6a5708f6ab5af7c56bd857b85347c39f24c83259f63455804e350050b88ac5208091564c63d3615d29e679008af8a4faeafb6795815d44dc0e7955789'
