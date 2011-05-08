29a6d948b32909d404d45815a01ebb0e5be4bb0d
German Garcia <german.garcia.d@gmail.com>
2011-05-08 09:47:02 -0500

    Fixed respon_to block

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,11 +1,23 @@
 class StationsController < ApplicationController
+  respond_to :json, :html
+
   def index
     @stations = Station.all
-    render :json => @stations
+    respond_with(@stations) do |format|
+      format.html
+      format.json { render :json => @stations }
+      # format.xml  { render :xml  => @stations }
+      # format.any  { render :text => "only HTML, XML, and JSON format are supported at the moment." }
+    end
   end
 
   def show
     @station = Station.where(:id => params[:id])
-    render :json => @station
+    respond_with(@station) do |format|
+      format.html
+      format.json { render :json => @station }
+      # format.xml  { render :xml  => @station }
+      # format.any  { render :text => "only HTML, XML, and JSON format are supported at the moment." }
+    end
   end
 end
